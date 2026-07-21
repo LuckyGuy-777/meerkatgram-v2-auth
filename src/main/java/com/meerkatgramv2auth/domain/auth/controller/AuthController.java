0fb766c1a4ed7c61344df5b427015a6ee9d25519
LuckyGuy-777 <yusch98@naver.com>
2026-07-21 18:03:12 +0900

    add login & security

---
+++ src/main/java/com/meerkatgramv2auth/domain/auth/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.meerkatgramv2auth.domain.auth.controller;
+
+
+import com.meerkatgramv2auth.domain.auth.request.LoginRequestDTO;
+import com.meerkatgramv2auth.global.response.GlobalRes;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Tag(name = "인증 API", description = "인증 담당") // 스웨거 어노테이션
+@RestController // 컨트롤러 임을 알림
+@RequiredArgsConstructor // 생성자 자동생성
+@RequestMapping("/api/auth")
+public class AuthController {
+
+
+    @PostMapping("/login")
+    public ResponseEntity<GlobalRes<Void>> login(
+        @Valid @RequestBody LoginRequestDTO loginRequestDTO,
+        HttpServletResponse response
+    ) {
+        return ResponseEntity.ok(GlobalRes.success());
+    }
+}
---
+++ src/main/java/com/meerkatgramv2auth/global/security/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.meerkatgramv2auth.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity // 메소드 레벨에서, 권한제어 활성화
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 스프링 시큐리티
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) // 화면 생성 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) // 폼로그인 기능 비활성화
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 토큰 인증 비활성화
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll()) // 인증 여부와 무관하게, 모든요청 통과.
+                .build();
+    }
+}
