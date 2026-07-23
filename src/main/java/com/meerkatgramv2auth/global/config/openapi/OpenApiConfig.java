c6e4294a9fa2a4d992596ba993c656ee80fc9b66
LuckyGuy-777 <yusch98@naver.com>
2026-07-23 17:38:34 +0900

    add ApiResponseCustomizer, CustomApiResponse, OpenApiConfig 추가

---
+++ src/main/java/com/meerkatgramv2auth/global/config/openapi/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.meerkatgramv2auth.global.config.openapi;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    private static final String BEARER_AUTH = "bearerAuth"; // 토큰인증방식으로 스웨거에서 체크할수 있음.
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Meerkatgram API") // 문서 제목
+                        .description("Meerkatgram REST API 문서입니다.") // 문서 설명
+                        .version("1.0.0") // 문서 버전
+                )
+                .components(new Components().addSecuritySchemes(BEARER_AUTH,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
+    }
+}
